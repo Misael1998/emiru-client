4e239d24ef927f4345fe60f6d2e33e23abf21342
Misael <misalande@gmail.com>
2020-10-08 11:37:45 -0600

    seccion de planes

--- src/app/modules/landing/landing.module.ts
+++ src/app/modules/landing/landing.module.ts
@@ -6,6 +6,7 @@ import { LandingComponent } from './components/landing/landing.component';
 import { LandingNavComponent } from './components/landing-nav/landing-nav.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
+import { PlansComponent } from './components/plans/plans.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { RegisterComponent } from './views/register/register.component';
     LandingNavComponent,
     LoginComponent,
     RegisterComponent,
+    PlansComponent,
   ],
   imports: [CommonModule, LandingRoutingModule],
   exports: [LandingComponent],
