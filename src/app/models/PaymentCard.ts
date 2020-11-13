2b73401801db3fbc6eaa1e86f2168b9901e65b7f
Misael <misalande@gmail.com>
2020-11-13 11:27:39 -0600

    card date validation

---
+++ src/app/models/PaymentCard.ts
@@ -0,0 +1,6 @@
+export interface PaymentCard {
+  name: string;
+  number: number | string;
+  expiration: number | string;
+  cvv: number | string;
+}
--- src/app/modules/landing/components/add-payment/add-payment.component.ts
+++ src/app/modules/landing/components/add-payment/add-payment.component.ts
@@ -1,4 +1,11 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  OnInit,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { PaymentCard } from 'src/app/models/PaymentCard';
 
 @Component({
   selector: 'app-add-payment',
@@ -7,6 +14,13 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 })
 export class AddPaymentComponent implements OnInit {
   @ViewChild('paymentFrom') paymentForm: any;
+  @Output() submitCard: EventEmitter<PaymentCard> = new EventEmitter();
+  card: PaymentCard = {
+    name: '',
+    number: '',
+    expiration: '',
+    cvv: '',
+  };
 
   constructor() {}
 
@@ -14,6 +28,45 @@ export class AddPaymentComponent implements OnInit {
 
   onsubmit(form: any, e: any) {
     e.preventDefault();
+    if (form.status == 'INVALID') {
+      alert('Asegurese de llenar el formulario con valores validos');
+      return;
+    }
+
+    const { cardName, cardNumber, cardExpiration, cardCvv } = form.value;
+
+    if (!this.dateIsValid(cardExpiration)) {
+      alert('invalid date');
+      return;
+    }
+
     console.log(form);
+    this.submitCard.emit({
+      name: cardName,
+      number: cardNumber,
+      expiration: cardExpiration,
+      cvv: cardCvv,
+    });
+  }
+
+  private dateIsValid(exp: string): boolean {
+    let expArr = exp.split('');
+    let date = new Date(Date.now());
+    let yy = parseInt(`20${expArr[2]}${expArr[3]}`);
+    let mm = parseInt(`${expArr[0]}${expArr[1]}`);
+
+    if (mm > 12 || mm <= 0) {
+      return false;
+    }
+
+    if (yy < date.getFullYear()) {
+      return false;
+    }
+
+    if (yy == date.getFullYear() && mm <= date.getMonth()) {
+      return false;
+    }
+
+    return true;
   }
 }
